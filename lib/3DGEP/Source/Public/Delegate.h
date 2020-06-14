18110dda8225b6af2bafae07cd6cb1cd34043a78
Riccardo Loggini <riccardo.loggini@outlook.com>
2020-06-14 19:46:46 +0100

    Multicast Variadic Template Delegate

--- lib/3DGEP/Source/Public/Delegate.h
+++ lib/3DGEP/Source/Public/Delegate.h
@@ -47,5 +47,58 @@ namespace GEPUtils {
 
 	};
 
+	/** Multicast, Variadic Template Delegate Class
+	*	Inspired by: https://www.codeproject.com/Articles/1170503/The-Impossibly-Fast-Cplusplus-Delegates-Fixed
+	*	My version can be used only with member functions and does not take return types into consideration
+	*	because I do not currently need such additional features.
+	*
+	*	Example of usage with a parameterless member function:
+	*
+	*	GEPUtils::MulticastDelegate myDelegate;
+	*	myDelegate.Add<Part2, &Part2::Run>(Part2::Get());
+	*	myDelegate.Broadcast();
+	*/
+	template <typename ...PARAMS>
+	class MulticastDelegate
+	{
+	public:
+
+		template <class T, void (T::* InMemberFn)(PARAMS...)>
+		void Add(T* InObj)
+		{
+			m_InvocationList.push_back(typename InvocationElement(InObj, CreateMemberFnStub<T, InMemberFn>));
+		}
+
+		void Broadcast(PARAMS... InArgs) const 
+		{
+			for (auto& CurrentInvocElement : m_InvocationList)
+			{
+				// Call the function, on the corresponding object, with the given params
+				(*(CurrentInvocElement.Stub))(CurrentInvocElement.ObjPtr, InArgs...); 
+			}
+		}
+
+	private:
+
+		using StubType = void (*)(void* InObjPtr, PARAMS...);
+
+		template <class T, void (T::* InMemberFn)(PARAMS...)>
+		static void CreateMemberFnStub(void* InObj, PARAMS... InParams)
+		{
+			T* objCastedT = static_cast<T*>(InObj);
+			return (objCastedT->*InMemberFn)(InParams...);
+		}
+		
+		struct InvocationElement
+		{
+			InvocationElement() = default;
+			InvocationElement(void* InObjPtr, StubType InStub) : ObjPtr(InObjPtr), Stub(InStub) { };
+			void* ObjPtr = nullptr;
+			StubType Stub = nullptr;
+		};
+
+		std::list<typename InvocationElement> m_InvocationList;
+	};
+
 }
 #endif // Delegate_h__
