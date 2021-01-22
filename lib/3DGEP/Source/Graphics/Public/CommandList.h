a4e8c6a5f450c4db7a7a78f11294a1864c904afd
Riccardo Loggini <riccardo.loggini@outlook.com>
2021-01-22 20:12:45 +0000

    Make All Methods To Be Abstract

--- lib/3DGEP/Source/Graphics/Public/CommandList.h
+++ lib/3DGEP/Source/Graphics/Public/CommandList.h
@@ -12,47 +12,47 @@ namespace GEPUtils { namespace Graphics {
 	{
 	public:
 
-		virtual void Close() { };
+		virtual void Close() = 0;
 
 		virtual void ResourceBarrier(GEPUtils::Graphics::Resource& InResource,
-			GEPUtils::Graphics::RESOURCE_STATE InPrevState, GEPUtils::Graphics::RESOURCE_STATE InAfterState) { }
+			GEPUtils::Graphics::RESOURCE_STATE InPrevState, GEPUtils::Graphics::RESOURCE_STATE InAfterState) = 0;
 
-		virtual void ClearRTV(GEPUtils::Graphics::CpuDescHandle& InDescHandle, float* InColor) { }
+		virtual void ClearRTV(GEPUtils::Graphics::CpuDescHandle& InDescHandle, float* InColor) = 0;
 		
-		virtual void ClearDepth(GEPUtils::Graphics::CpuDescHandle& InDescHandle) { }
+		virtual void ClearDepth(GEPUtils::Graphics::CpuDescHandle& InDescHandle) = 0;
 
-		virtual void SetPipelineStateAndResourceBinder(GEPUtils::Graphics::PipelineState& InPipelineState) { }
+		virtual void SetPipelineStateAndResourceBinder(GEPUtils::Graphics::PipelineState& InPipelineState) = 0;
 
-		virtual void SetInputAssemblerData(GEPUtils::Graphics::PRIMITIVE_TOPOLOGY InPrimTopology, GEPUtils::Graphics::VertexBufferView& InVertexBufView, GEPUtils::Graphics::IndexBufferView& InIndexBufView) { }
+		virtual void SetInputAssemblerData(GEPUtils::Graphics::PRIMITIVE_TOPOLOGY InPrimTopology, GEPUtils::Graphics::VertexBufferView& InVertexBufView, GEPUtils::Graphics::IndexBufferView& InIndexBufView) = 0;
 
-		virtual void SetViewportAndScissorRect(GEPUtils::Graphics::ViewPort& InViewport, GEPUtils::Graphics::Rect& InScissorRect) { }
+		virtual void SetViewportAndScissorRect(GEPUtils::Graphics::ViewPort& InViewport, GEPUtils::Graphics::Rect& InScissorRect) = 0;
 
-		virtual void SetRenderTargetFromWindow(GEPUtils::Graphics::Window& InWindow) { }
+		virtual void SetRenderTargetFromWindow(GEPUtils::Graphics::Window& InWindow) = 0;
 
-		virtual void SetGraphicsRootConstants(uint64_t InRootParameterIndex, uint64_t InNum32BitValuesToSet, const void* InSrcData, uint64_t InDestOffsetIn32BitValues) { }
+		virtual void SetGraphicsRootConstants(uint64_t InRootParameterIndex, uint64_t InNum32BitValuesToSet, const void* InSrcData, uint64_t InDestOffsetIn32BitValues) = 0;
 
-		virtual void SetComputeRootConstants(uint64_t InRootParameterIndex, uint64_t InNum32BitValuesToSet, const void* InSrcData, uint64_t InDestOffsetIn32BitValues) { }
+		virtual void SetComputeRootConstants(uint64_t InRootParameterIndex, uint64_t InNum32BitValuesToSet, const void* InSrcData, uint64_t InDestOffsetIn32BitValues) = 0;
 
-		virtual void SetGraphicsRootTable(uint32_t InRootIndex, GEPUtils::Graphics::ConstantBufferView& InView) { }
+		virtual void SetGraphicsRootTable(uint32_t InRootIndex, GEPUtils::Graphics::ConstantBufferView& InView) = 0;
 
-		virtual void DrawIndexed(uint64_t InIndexCountPerInstance) { }
+		virtual void DrawIndexed(uint64_t InIndexCountPerInstance) = 0;
 
-		virtual void Dispatch(uint32_t InGroupsNumX, uint32_t InGroupsNumY, uint32_t InGroupsNumZ) { }
+		virtual void Dispatch(uint32_t InGroupsNumX, uint32_t InGroupsNumY, uint32_t InGroupsNumZ) = 0;
 
-		virtual void UploadViewToGPU(GEPUtils::Graphics::ShaderResourceView& InSRV) { }
+		virtual void UploadViewToGPU(GEPUtils::Graphics::ShaderResourceView& InSRV) = 0;
 
-		virtual void UploadUavToGpu(GEPUtils::Graphics::UnorderedAccessView& InUav) { }
+		virtual void UploadUavToGpu(GEPUtils::Graphics::UnorderedAccessView& InUav) = 0;
 
-		virtual void StoreAndReferenceDynamicBuffer(uint32_t InRootIdx, GEPUtils::Graphics::DynamicBuffer& InDynBuffer, GEPUtils::Graphics::ConstantBufferView& InResourceView) { }
+		virtual void StoreAndReferenceDynamicBuffer(uint32_t InRootIdx, GEPUtils::Graphics::DynamicBuffer& InDynBuffer, GEPUtils::Graphics::ConstantBufferView& InResourceView) = 0;
 
-		virtual void ReferenceSRV(uint32_t InRootIdx, GEPUtils::Graphics::ShaderResourceView& InSRV) { }
+		virtual void ReferenceSRV(uint32_t InRootIdx, GEPUtils::Graphics::ShaderResourceView& InSRV) = 0;
 
-		virtual void ReferenceComputeTable(uint32_t InRootIdx, GEPUtils::Graphics::ShaderResourceView& InUav) { }
+		virtual void ReferenceComputeTable(uint32_t InRootIdx, GEPUtils::Graphics::ShaderResourceView& InUav) = 0;
 
-		virtual void ReferenceComputeTable(uint32_t InRootIdx, GEPUtils::Graphics::UnorderedAccessView& InUav) { }
+		virtual void ReferenceComputeTable(uint32_t InRootIdx, GEPUtils::Graphics::UnorderedAccessView& InUav) = 0;
 
 		// Internally calls ::UpdateSubresources(..) where IntermediateBuffer is expected to be allocated in upload heap
-		virtual void UploadBufferData(GEPUtils::Graphics::Buffer& DestinationBuffer, GEPUtils::Graphics::Buffer& IntermediateBuffer, const void* InBufferData, size_t InDataSize) { }
+		virtual void UploadBufferData(GEPUtils::Graphics::Buffer& DestinationBuffer, GEPUtils::Graphics::Buffer& IntermediateBuffer, const void* InBufferData, size_t InDataSize) = 0;
 
 	protected:
 		CommandList(GEPUtils::Graphics::Device& InDevice);
